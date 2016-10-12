8e93280c807d6699fa305f23c78adf391d0aab05
David Johnson <dhjohn0@gmail.com>
2016-10-12 10:14:40 -0500

    Respond with view formWardenError

--- middleware/form_warden_middleware.js
+++ middleware/form_warden_middleware.js
@@ -4,16 +4,14 @@ var serialize = require('serialize-javascript');
 module.exports = (validationOptions) => {
   var str = serialize(validationOptions);
 
-  return (req, locals, next) => {
+  return (req, locals, respond, views, _, next) => {
     locals.validationOptions = req.validationOptions = str;
-    
+
     let result = formWarden.validateForm(req.params, validationOptions);
     if (!result.validForm) {
-      return {
-        status: 500,
-        headers: {},
-        body: ['Invalid Form!']
-      };
+      var response = respond(views.formWardenError, { result: result });
+      response.status = 500;
+      return response;
     }
 
     return next();
