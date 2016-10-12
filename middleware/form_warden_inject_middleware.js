e53f4d538e5d3780f4b665947cad43222a520673
David Johnson <dhjohn0@gmail.com>
2016-10-12 09:46:38 -0500

    Inject validationOptions into locals

--- middleware/form_warden_inject_middleware.js
+++ middleware/form_warden_inject_middleware.js
@@ -1,4 +1,3 @@
-var formWarden = require('form-warden');
 var serialize = require('serialize-javascript');
 
 module.exports = (validationOptions) => {
@@ -6,16 +5,6 @@ module.exports = (validationOptions) => {
 
   return (req, locals, next) => {
     locals.validationOptions = req.validationOptions = str;
-    
-    let result = formWarden.validateForm(req.params, validationOptions);
-    if (!result.validForm) {
-      return {
-        status: 500,
-        headers: {},
-        body: ['Invalid Form!']
-      };
-    }
-
     return next();
   };
 }
