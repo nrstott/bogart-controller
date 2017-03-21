f598725e3e1afd2512f2f609247834754144bd97
David Johnson <dhjohn0@gmail.com>
2017-03-21 10:08:34 -0500

    Flatten forms on backend to match frontend validation for form-warden

--- middleware/form_warden_middleware.js
+++ middleware/form_warden_middleware.js
@@ -5,9 +5,35 @@ module.exports = (validationOptions) => {
   var str = serialize(validationOptions);
 
   return (req, locals, respond, views, _, next) => {
+    function flatten(obj, notFirst) {
+      var newObj = {};
+
+      if (Array.isArray(obj)) {
+        _.each(obj, (val, index) => {
+          var result = flatten(val, true);
+          _.forOwn(result, (v, k) => {
+            newObj[notFirst ? '[' + index + ']' + k : index + k] = v;
+          });
+        });
+      }else if (typeof(obj) === 'object') {
+        _.forOwn(obj, (val, key) => {
+          var result = flatten(val, true);
+          _.forOwn(result, (v, k) => {
+            newObj[notFirst ? '[' + key + ']' + k : key + k] = v;
+          });
+        });
+      }else{
+        newObj[''] = obj;
+      }
+
+      return newObj;
+    }
+
     locals.validationOptions = req.validationOptions = str;
 
-    let result = formWarden.validateForm(req.params, validationOptions);
+    var formObj = flatten(req.params);
+
+    let result = formWarden.validateForm(formObj, validationOptions);
     if (!result.validForm) {
       var response = respond(views.formWardenError, { result: result });
       response.status = 500;
